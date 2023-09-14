02b811b72d5dc37a4780155e7bd373b15a80d36b
srajan-kush <144719261+srajan-kush@users.noreply.github.com>
2023-09-14 22:31:31 +0530

    updated

--- 11_bTree.c
+++ 11_bTree.c
@@ -20,38 +20,65 @@ struct node *create(){
     newnode->right = create();
     return newnode;
 }
-int* inorderTraversal(struct node* root, int* returnSize){
-      if(root == NULL){
-          return returnSize;
-      }
-      inorderTraversal(root->left,returnSize);
-      static int i = 0;
-      *(returnSize+i) = root->data;
-      i = i+1;
-      inorderTraversal(root->right,returnSize);
+
+void preorderTraverse(struct node* root){
+    if(root==NULL)
+      return;
+    printf("%d ",root->data);
+    preorderTraverse(root->left);
+    preorderTraverse(root->right);
+}
+void inorderTraverse(struct node* root){
+    if(root==NULL)
+      return;
+    inorderTraverse(root->left);
+    printf("%d ",root->data);
+    inorderTraverse(root->right);
 }
 
-int fun(struct node* head){
-    
-    if(head==NULL){
-        return 0;
-    }
-    if(head->left==NULL && head->right==NULL){
-        return 1;
-    }
-    return (fun(head->right)+fun(head->left));
+void postorderTraverse(struct node* root){
+    if(root==NULL)
+      return;
+    postorderTraverse(root->left);
+    postorderTraverse(root->right);
+    printf("%d ",root->data);
 }
 
 int main(){
     struct node *root;
     root = NULL;
     root = create();
-    int a[20];
-    int *b = inorderTraversal(root,&a);
-    for(int i =0;i<3;i++){
-        printf("%d ",*(b+i));
-    }
-    
-
-
+    preorderTraverse(root);
+    printf("\n");
+    inorderTraverse(root);  
+    printf("\n");
+    postorderTraverse(root);
 }
+/*
+Enter data: 9
+Enter data: 8
+Enter data: 7
+Enter data: 2
+Enter data: -1
+Enter data: -1
+Enter data: 1
+Enter data: -1
+Enter data: -1
+Enter data: 3
+Enter data: -1
+Enter data: -1
+Enter data: 2
+Enter data: -1
+Enter data: 5
+Enter data: 9
+Enter data: 6
+Enter data: -1
+Enter data: -1
+Enter data: -1
+Enter data: 8
+Enter data: -1
+Enter data: -1
+9 8 7 2 1 3 2 5 9 6 8  #preorder
+2 7 1 8 3 9 2 6 9 5 8  #inorder
+2 1 7 3 8 6 9 8 5 2 9  #postorder
+*/
